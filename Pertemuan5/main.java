6a39db85fe3ea82d35a298245cc9511059c49187
MalmediraFisyara <145527581+MalmediraFisyara@users.noreply.github.com>
2023-10-26 21:11:17 +0700

    Pertemuan5

---
+++ Pertemuan5/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan5;
+
+/**
+ *
+ * @author SST Sang Surya
+ */
+public class main {
+    public static void main(String[] args) {
+        
+        MobilMatic matic = new MobilMatic();
+        matic.jalankan();
+    }
+}
